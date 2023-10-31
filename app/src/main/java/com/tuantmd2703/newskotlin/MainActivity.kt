102126aedeb5ca9aa6a788916864cbeb48ad94d7
tuantm2703 <tuantmd2703@gmail.com>
2023-10-31 14:51:39 +0700

    splash

--- app/src/main/java/com/tuantmd2703/newskotlin/MainActivity.kt
+++ app/src/main/java/com/tuantmd2703/newskotlin/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tuantmd2703.newskotlin.ui.theme.NewsKotlinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NewsKotlinTheme {
                 // A surface container using the 'background' color from the theme
